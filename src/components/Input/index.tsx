3095e95fdf4330da83797ac746162803e7c72c80
Kaio <kaio_matos22@hotmail.com>
2021-11-12 15:26:40 -0300

    Changing again font family and removing spellcheck

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,6 +8,7 @@ export default function Input({ setText }: InputPropsType) {
   return (
     <div className="input_container">
       <textarea
+        spellCheck="false"
         onChange={(e) => {
           setText(e.currentTarget.value);
         }}
